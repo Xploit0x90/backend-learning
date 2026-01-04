176c81212ad09a7272a2ef88a6818ce5fd66705a
Mohammad Sarhan <mohammad.sarhan@stud.h-da.de>
2026-01-04 17:54:50 +0100

    Added environment variables with dotenv

--- 02-todo-api/app.js
+++ 02-todo-api/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const todoRoutes = require("./routes/todos");
 const {logger} = require("./middleware/logger.js");
@@ -8,12 +10,13 @@ const app = express();
 
 app.use(logger);
 app.use(timer);
-
 app.use(express.json());
 app.use("/todos", todoRoutes);
-
 app.use(errorHandler);
 
-app.listen(3002, ()=>{
-    console.log("Listening on Port 3002");
-})
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+    console.log(`${process.env.APP_NAME} listening on port ${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`);
+});
